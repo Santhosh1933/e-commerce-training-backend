384f9aafb5c1c0c73a30826602b2857b84ab476c
Santhosh <santhoshs19032003@gmail.com>
2024-05-12 16:33:13 +0530

    API setup

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("san")
+// import packages -> require
+const express = require("express");
+
+const app = express();
+
+// create server
+app.listen(8000, () => {
+  console.log("http:localhost:8000");
+});
+
+// get -> send details from server-client eg:-login
+// post -> eg:- registration ( client -> server -> db update (create))
+
+
+// req->from user (client) 
+// res-> to user ( server->client)
+
+// 200 -> ok
+// 404 -> not found
+// 501 -> server error
+
+// initial route (Home Route)
+app.get("/", (req, res) => {
+    return res.send("Hello world").status(200)
+});
+app.get("/user", (req, res) => {
+    return res.send("santhosh").status(200)
+});
